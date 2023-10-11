3ba4f441dde9bbc966b4f00bc23fb311bc185458
Sean Golden <sean.golden.one@gmail.com>
2023-10-12 00:20:50 +0100

    Minor Update

--- src/app/members/mermber-list/mermber-list.component.ts
+++ src/app/members/mermber-list/mermber-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-mermber-list',
   templateUrl: './mermber-list.component.html',
   styleUrls: ['./mermber-list.component.css']
 })
-export class MermberListComponent {
+export class MermberListComponent implements OnInit{
+  members: Member[] = [];
+
+  constructor(private memberService:MembersService){}
+
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers(){
+    this.memberService.getMembers().subscribe({
+      next: members=> this.members = members
+    })
+  }
 
 }
